0dce7d96673fc39752d1bac72c6079e64a2d4734
wing <1531780174@qq.com>
2019-06-10 13:58:33 +0800

    make-redux框架搭建

---
+++ make-redux/src/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Header extends Component {
+  render () {
+    return (
+      <h1>make redux ---- header</h1>
+    )
+  }
+}
+
+export default Header
---
+++ make-redux/src/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import Header from './Header'
+import Content from './Content'
+
+class Index extends Component {
+  render () {
+    return (
+      <div>
+        <Header/>
+        <Content/>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
